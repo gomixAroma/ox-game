0630560bea9768b6b8abb9afc74e78c3f0447bf6
Aroma <134286733+gomixAroma@users.noreply.github.com>
2024-04-18 17:53:52 +0900

    envを設定

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,19 +1,17 @@
-/* eslint-disable no-undef */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: FIREBASE_APIKEY,
-    authDomain: FIREBASE_DOMAIN,
-    projectId: FIREBASE_PROJECT_ID,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: FIREBASE_SENDER_ID,
-    appId: FIREBASE_APP_ID
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    ENV_API_KEY: JSON.stringify('KEY:XXX-XXX'),
-    FIREBASE_APIKEY: JSON.stringify("AIzaSyAkjhiaXchK_GyFO0KWW3U7yYFQTp9_dCg"),
-    FIREBASE_DOMAIN: JSON.stringify("ox-game-723da.firebaseapp.com"),
-    FIREBASE_PROJECT_ID: JSON.stringify("ox-game-723da"),
-    FIREBASE_STORAGE_BUCKET: JSON.stringify("ox-game-723da.appspot.com"),
-    FIREBASE_SENDER_ID: JSON.stringify("452868623343"),
-    FIREBASE_APP_ID: JSON.stringify("1:452868623343:web:5c6780b08e8c0c218bc41a"),
-
-  },
+  envDir: './src/',
 })
